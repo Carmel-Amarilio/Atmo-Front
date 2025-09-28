dc7794c5c7c387a3a834e39e08a60d6861e79455
Carmel-Amarilio <carmelamarilio9@gmail.com>
2025-09-28 20:19:23 +0300

    add GH

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Atmo-front",
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@emotion/react', '@emotion/styled'],
+  },
 })
