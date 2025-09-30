6794b6ed0bfe81527c6fe758dd9493250994db7f
Carmel-Amarilio <carmel.amarilio@tufin.com>
2025-09-30 11:48:43 +0300

    add AtmoAction view

--- src/views/AtmoAction.jsx
+++ src/views/AtmoAction.jsx
@@ -1,8 +1,35 @@
+import LogoImg from '../assets/img/Logo1.png'
+import icon1Img from '../assets/img/icon1.png'
+import icon2Img from '../assets/img/icon2.png'
+import icon3Img from '../assets/img/icon3.png'
 
 export function AtmoAction() {
     return (
-        <section className='atmo-action flex column justify-center align-center '>
-            
+        <section className='atmo-action flex column align-center '>
+            <article className='main-logo flex column align-center justify-center gap10' >
+                <div className='flex align-center justify-center gap10'>
+                    <img src={LogoImg} alt="" />
+                    <h1>Atmo</h1>
+                </div>
+            </article>
+
+            <section className='option flex align-center justify-center gap10'>
+                <article className='action-card flex column align-center justify-center gap20'>
+                    <img src={icon1Img} alt="" />
+                    <h2>Cost Savings</h2>
+                    <p>Get recommendations on how to reduce cloud costs.</p>
+                </article>
+                <article className='action-card flex column align-center justify-center gap20'>
+                    <img src={icon2Img} alt="" />
+                    <h2>Log Analysis</h2>
+                    <p>Gain insights from cloud logs to monitor and troubleshoot.</p>
+                </article>
+                <article className='action-card flex column align-center justify-center gap20'>
+                    <img src={icon3Img} alt="" />
+                    <h2>Cloud Consultation</h2>
+                    <p>Ask questions and get advice on your cloud environment.</p>
+                </article>
+            </section>
         </section>
     )
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,31 @@
-import iconImg from '../assets/img/icon1.png'
+import { useNavigate } from 'react-router-dom';
+
+import LogoImg from '../assets/img/Logo1.png'
 import AtmoGraf from '../assets/img/AtmoGraf.png'
 
 export function Home() {
+    const navigate = useNavigate();
+
+    function goToAction() {
+        navigate('/action');
+    }
+
     return (
         <section className='home flex column justify-center align-center '>
-            <article className='main-logo flex align-center justify-center gap10' >
-                <img src={iconImg} alt="" />
-                <h1>Atmo</h1>
+            <article className='main-logo flex column align-center justify-center gap10' >
+                {/* <div className='flex align-center justify-center gap10'> */}
+                    <img src={LogoImg} alt="" />
+                    <h1>Atmo</h1>
+                {/* </div> */}
+                <h2>the clear sky for your cloud</h2>
             </article>
+
             <article className='scend-logo'>
                 <img src={AtmoGraf} alt="" />
                 <div className='flex column gap20 justify-center'>
                     <h2>Take control of your cloud infrastructure</h2>
                     <p>Atmo collects data across multi - cloud platforms, provides improved visualizations.AI enhancements, and actionable recommendations.</p>
-                    <button>Get Started</button>
+                    <button onClick={goToAction}>Get Started</button>
                 </div>
             </article>
         </section>
